bb6d0ae75c709181fe83ebfce06bc0973bd38be0
davidwillson9 <2021.aniket.mahajan@ves.ac.in>
2025-06-18 15:13:49 +0530

    Model update

--- Backend/model/price/font.js
+++ Backend/model/price/font.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const FontSchema = new mongoose.Schema({
-  size: { type: String, required: true }, 
+  name: { type: String, required: true },
   price: { type: Number, required: true }
 });
 
