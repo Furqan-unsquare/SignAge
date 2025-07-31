ae2a2656c6881b1c48c045163905ee09fa599b0d
Furqan Ansari <ansarifurkanabcd786@gmail.com>
2025-07-31 17:10:17 +0530

    Allow Cors

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const signageTypeRoutes = require('./routes/signageTypeRoutes');
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://acrylicsignboards.in/', 'https://acrylicsignboards-admin.in/'],
+  origin: ['http://localhost:5173', 'https://acrylicsignboards.in/', 'https://acrylicsignboards-admin.netlify.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
