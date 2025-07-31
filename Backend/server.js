eaf4fadd6ecd1bd8dd3fba0bf0326bbcf3bf8926
Furqan Ansari <ansarifurkanabcd786@gmail.com>
2025-07-31 16:38:37 +0530

    Changes done

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,13 @@ const configRoutes = require('./routes/configRoutes');
 const signageTypeRoutes = require('./routes/signageTypeRoutes');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://acrylicsignboards.in/', 'https://acrylicsignboards-admin.in/'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -45,7 +51,6 @@ app.use('/fonts', express.static(path.join(__dirname, 'fonts'), {
   }
 }));
 app.use("/api/font-files", require("./routes/fontRoutes")); // mount it
-
 app.use('/api', configRoutes);
 app.use('/api/signage-types', signageTypeRoutes);
 
